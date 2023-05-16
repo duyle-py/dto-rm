4c613e57a6e2de169bc55ec6942071a4875cbd5b
duyle-py <duyltk.cs@gmail.com>
2023-05-16 22:45:37 +0700

    make ex - not generics

---
+++ dtos/gen_proto.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+set -e
+python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. *.proto
---
+++ dtos/grpc.py
@@ -0,0 +1,7 @@
+from dtos.dto import DTO
+
+class gRPC(DTO):
+    pass
+
+class AnExampleDTO (gRPC):
+    pass
